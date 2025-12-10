9b352a90d5756b1a95bcbbd2b8c39e6ec28d5b54
Lincoln Li <li_yongkun@visualon.com>
2025-12-10 18:21:28 +0800

    更新实验代码

--- yobot_paint.cpp
+++ yobot_paint.cpp
@@ -38,14 +38,26 @@ namespace yobot {
     }
     int paint::draw()
     {
-        SDL_SetRenderDrawColor(m_renderer.get(), 0, 0, 0, 0);
+        SDL_SetRenderDrawColor(m_renderer.get(), 255, 255, 255, 255);
         SDL_RenderClear(m_renderer.get());
-        auto iStream = unique_sdl_iostream(SDL_IOFromFile("icon/312501.webp", "r"));
-        auto iSurface = unique_sdl_surface(IMG_LoadWEBP_IO(iStream.get()));
-        auto texture = unique_sdl_texture(SDL_CreateTextureFromSurface(m_renderer.get(), iSurface.get()));
-        SDL_RenderTexture(m_renderer.get(), texture.get(), NULL, NULL);
+        SDL_SetRenderDrawColor(m_renderer.get(), 0, 0, 0, 128);
+        auto targetRect = SDL_FRect{ 10.0f,30.0f,460.0f,580.0f };
+        SDL_RenderRect(m_renderer.get(), &targetRect);
+        auto texture = unique_sdl_texture(IMG_LoadTexture(m_renderer.get(), "icon/312501.webp"));
+        targetRect = SDL_FRect{ 20.0f,20.0f,(float)(texture->w / 8 * 5),(float)(texture->h / 8 * 5) };
+        auto HPRect = SDL_FRect{ 40.0f + targetRect.w,0,targetRect.w * 4,targetRect.h / 3 };
+        for (int i = 0; i < 5; i++)
+        {
+            targetRect.y = (float)(110 + 100 * i);
+            SDL_RenderLine(m_renderer.get(), 10.0f, targetRect.y, 469.0f, targetRect.y);
+            targetRect.y += 10.0f;
+            SDL_RenderTexture(m_renderer.get(), texture.get(), nullptr, &targetRect);
+            HPRect.y = targetRect.y + targetRect.h / 3;
+            SDL_RenderRect(m_renderer.get(), &HPRect);
+        }
         auto oSurface = unique_sdl_surface(SDL_RenderReadPixels(m_renderer.get(), nullptr));
         IMG_SavePNG(oSurface.get(), "test.png");
+        //SDL_RenderPresent(m_renderer.get());
         return 0;
     }
 }
