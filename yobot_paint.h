a2ae8d805597d0729492bcf9197bfae0b4677225
Lincoln Li <li_yongkun@visualon.com>
2025-12-12 18:41:51 +0800

    更新实验代码

--- yobot_paint.h
+++ yobot_paint.h
@@ -2,6 +2,7 @@
 #include <memory>
 #include <SDL3/SDL_video.h>
 #include <SDL3/SDL_render.h>
+#include <SDL3_ttf/SDL_textengine.h>
 
 namespace yobot {
     class paint
@@ -18,6 +19,7 @@ namespace yobot {
     private:
         std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> m_window;
         std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> m_renderer;
+        std::unique_ptr<TTF_TextEngine, decltype(&TTF_DestroyRendererTextEngine)> m_textEngine;
     };
 }
 
